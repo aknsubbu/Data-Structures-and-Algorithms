6ccadbfbc824e68c02cc306fb23e8941916abe68
Anandkumar NS <69028746+aknsubbu@users.noreply.github.com>
2023-08-16 05:51:17 +0000

    anand bubble-sort

---
+++ Anandkumar NS/Sorting/bubble_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+    int n;
+    printf("Enter the number of elements to be sorted: ");
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+
+    // Input
+    for(int i = 0; i < n; ++i){
+        printf("Enter element number %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    // Printing
+    printf("Sorted elements:\n");
+    for(int i = 0; i < n; ++i){
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+
+    return 0;
+}
+
+void bubbleSort(int *arr, int n){
+    for (int c = 0; c < n - 1; ++c) {
+        for (int x = 0; x < n - 1 - c; ++x) {
+            if (arr[x] > arr[x + 1]) {
+                swap(&arr[x], &arr[x + 1]);
+            }
+        }
+    }
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
