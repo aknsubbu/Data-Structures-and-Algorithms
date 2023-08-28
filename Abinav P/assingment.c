861757aca7177d537465584898d9602c874ef87c
Userded-19 <abinav10102@gmail.com>
2023-08-28 18:09:47 +0530

    end of 28-8

--- Abinav P/assingment.c
+++ Abinav P/assingment.c
@@ -20,21 +20,81 @@
 
 
 //insertion sort
+// #include<stdio.h>
+
 #include<stdio.h>
-void main(){
-    int array[]={5,3,1,4,2};
+#include<time.h>
+#include<stdlib.h>
+
+int *insertion(int sampleLengthVar, int* sampleArrayPtr){
+    
     int i,j,key;
-    for(i=1;i<5;i++){
-        key=array[i];
+    for(i=1;i<sampleLengthVar;i++){
+        key=sampleArrayPtr[i];
         j=i-1;
-        while(j>=0 && array[j]>key){
-            array[j+1]=array[j];
+        while(j>=0 && sampleArrayPtr[j]>key){
+            sampleArrayPtr[j+1]=sampleArrayPtr[j];
             j=j-1;
         }
-        array[j+1]=key;
+        sampleArrayPtr[j+1]=key;
     }
-    for(int i=0;i<5;i++){
-        printf("%d ",array[i]);
+    for(int i=0;i<sampleLengthVar;i++){
+        printf("%d ",sampleArrayPtr[i]);
     }
+    return sampleArrayPtr; 
+}
+
+
+
+int* readFile(char filename[]){
+    FILE *fp;
+    fp = fopen(filename, "r");
+    
+    int sampleLengthVar, sampleElement;
+
+    fscanf(fp, "%d", &sampleLengthVar);
+    printf("Scanning a sample of length: %d\n", sampleLengthVar);
+
+    printf("    ALLOC-PTR_START\n");
+    int* sampleArrayPtr = (int*)malloc(sampleLengthVar * sizeof(int));
+    printf("    ALLOC-PTR_END\n");
+
+    printf("    SCN-START\n");
+    for (int iterable = 0; iterable < sampleLengthVar; iterable++){
+        printf("    SCN-ITR: %d\r", iterable);
+        fscanf(fp, "%d", &sampleElement);
+        *(sampleArrayPtr + iterable) = sampleElement;
+    }
+    printf("\n");
+    printf("    SCN-END\n");
+
+    fclose(fp);
+
+    int *result=insertion(sampleArrayPtr, sampleLengthVar);
+    fp=fopen("insertion.txt","w");
+    for(int i=0;i<sampleLengthVar;i++){
+        fprintf(fp,"%d ",result[i]);
+    }
+    fclose(fp);
+    return 0;
+ }
+
+
+int run(char filename[]){
+    printf("Scanning for and sorting `%s` \n", filename);
+    clock_t start = clock(); // start the timer
+
+    readFile(filename);
+
+    clock_t end = clock(); // end the timer
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent: %f\n", time_spent);
+    printf("\n\n");
+}
+
+
+
+void main(){
+    run("./descending.txt");
 }
 
