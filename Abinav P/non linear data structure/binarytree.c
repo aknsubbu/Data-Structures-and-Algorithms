3b4e1439098d61367d2e398492bc817cf7fd7c07
Userded-19 <abinav10102@gmail.com>
2023-11-02 11:14:55 +0530

    02-11

--- Abinav P/non linear data structure/binarytree.c
+++ Abinav P/non linear data structure/binarytree.c
@@ -326,43 +326,68 @@ struct node *insert(struct node *root, int data) {
     return root;
 }
 
-struct node *delete(struct node *head,int data){
-    struct node *current=head;
-    if(head==NULL){
-        return NULL;
-    }
-    if(data<current->data){
-        current->left=delete(current->left,data);
-    }
-    else if(data>current->data){
-        current->right=delete(current->right,data);
-    }
-    else{
-        if(current->left==NULL && current->right==NULL){
-            free(current);
-            return NULL;
-        }
-        else if(current->left==NULL){
-            struct node *temp=current->right;
-            free(current);
-            return temp;
-        }
-        else if(current->right==NULL){
-            struct node *temp=current->left;
-            free(current);
-            return temp;
-        }
-        else{
-            struct node *temp=current->right;
-            while(temp->left!=NULL){
-                temp=temp->left;
-            }
-            current->data=temp->data;
-            current->right=delete(current->right,temp->data);
-        }
-    }
-}
 
+void delete(struct node *root, int data) {
+    struct node *current = root;
+    struct node *parent = NULL;
+    while (current != NULL && current->data != data) {
+        parent = current;
+        if (data < current->data) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    if (current == NULL) {
+        printf("Node to be deleted not found\n");
+        return;
+    }
+
+    // Case 1: Node to be deleted has no children
+    if (current->left == NULL && current->right == NULL) {
+        if (current != root) {
+            if (parent->left == current) {
+                parent->left = NULL;
+            } else {
+                parent->right = NULL;
+            }
+        } else {
+            root = NULL;
+        }
+        free(current);
+    }
+    // Case 2: Node to be deleted has one child
+    else if (current->left == NULL || current->right == NULL) {
+        struct node *child = current->left != NULL ? current->left : current->right;
+        if (current != root) {
+            if (current == parent->left) {
+                parent->left = child;
+            } else {
+                parent->right = child;
+            }
+        } else {
+            root = child;
+        }
+        free(current);
+    }
+    // Case 3: Node to be deleted has two children
+    else {
+        struct node *successor = current->right;
+        struct node *successor_parent = current;
+        while (successor->left != NULL) {
+            successor_parent = successor;
+            successor = successor->left;
+        }
+        if (successor_parent->left == successor) {
+            successor_parent->left = successor->right;
+        } else {
+            successor_parent->right = successor->right;
+        }
+        current->data = successor->data;
+        free(successor);
+    }
+}   
 
 struct node *search(int data,struct node *root){
     struct node *current=root;
@@ -415,3 +440,6 @@ void main(){
     }
 
 }
+
+
+//step1 find the inorder succesor of the node to be deleted
