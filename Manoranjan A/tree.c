382235cfc645bab641d4870c963bc216626ebb2d
Manoranjan-A <manospartan007@gmail.com>
2023-11-04 18:34:38 +0530

    Refactor binary tree implementation with struct and dynamic memory allocation

--- Manoranjan A/tree.c
+++ Manoranjan A/tree.c
@@ -1,178 +1,194 @@
-// traverse inorder the left sub tree
-// process root
-// traverse inorder the right sub tree
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
 
-void inorder(int *tree, int i)
-{
-    if(tree[i] != -1)
-    {
-        inorder(tree, 2*i+1);
-        printf("%d ", tree[i]);
-        inorder(tree, 2*i+2);
+typedef struct TreeNode TreeNode;
+
+TreeNode* insert(TreeNode* root, int n) {
+    if (root == NULL) {
+        TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+        newNode->data = n;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    if (n < root->data) {
+        root->left = insert(root->left, n);
+    } else if (n > root->data) {
+        root->right = insert(root->right, n);
+    }
+
+    return root;
+}
+
+TreeNode* search(TreeNode* root, int n) {
+    if (root == NULL || root->data == n) {
+        return root;
+    }
+
+    if (n < root->data) {
+        return search(root->left, n);
+    } else {
+        return search(root->right, n);
     }
 }
 
-void preorder(int *tree, int i)
-{
-    if(tree[i] != -1)
-    {
-        printf("%d ", tree[i]);
-        preorder(tree, 2*i+1);
-        preorder(tree, 2*i+2);
+TreeNode* findMin(TreeNode* node) {
+    while (node->left != NULL) {
+        node = node->left;
     }
+    return node;
 }
 
-void postorder(int *tree, int i)
-{
-    if(tree[i] != -1)
-    {
-        postorder(tree, 2*i+1);
-        postorder(tree, 2*i+2);
-        printf("%d ", tree[i]);
+TreeNode* delete(TreeNode* root, int n) {
+    if (root == NULL) {
+        return root;
     }
-}
 
-void insert(int *tree, int n, int i)
-{
-    if(tree[i] == -1)
-    {
-        tree[i] = n;
-        return;
-    }
-    if(n < tree[i])
-        insert(tree, n, 2*i+1);
-    else
-        insert(tree, n, 2*i+2);
-}
-
-void search(int *tree, int n, int i)
-{
-    if(tree[i] == -1)
-    {
-        printf("Not found\n");
-        return;
-    }
-    if(n == tree[i])
-    {
-        printf("Found\n");
-        return;
-    }
-    if(n < tree[i])
-        search(tree, n, 2*i+1);
-    else
-        search(tree, n, 2*i+2);
-}
-
-void delete(int *tree, int n, int i)
-{
-    if(tree[i] == -1)
-    {
-        printf("Not found\n");
-        return;
-    }
-    if(n == tree[i])
-    {
-        if(tree[2*i+1] == -1 && tree[2*i+2] == -1)
-        {
-            tree[i] = -1;
-            return;
+    if (n < root->data) {
+        root->left = delete(root->left, n);
+    } else if (n > root->data) {
+        root->right = delete(root->right, n);
+    } else {
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
         }
-        if(tree[2*i+1] == -1)
-        {
-            tree[i] = tree[2*i+2];
-            tree[2*i+2] = -1;
-            return;
-        }
-        if(tree[2*i+2] == -1)
-        {
-            tree[i] = tree[2*i+1];
-            tree[2*i+1] = -1;
-            return;
-        }
-        int j = 2*i+2;
-        while(tree[2*j+1] != -1)
-            j = 2*j+1;
-        tree[i] = tree[j];
-        delete(tree, tree[j], j);
-        return;
+
+        TreeNode* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, temp->data);
     }
-    if(n < tree[i])
-        delete(tree, n, 2*i+1);
-    else
-        delete(tree, n, 2*i+2);
+
+    return root;
 }
 
-void print(int *tree, int n)
-{
-    for(int i=0; i<n; i++)
-        printf("%d ", tree[i]);
+void inorder(TreeNode* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(TreeNode* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(TreeNode* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void freeTree(TreeNode* root) {
+    if (root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
+
+// Function to print the binary tree structure
+void printTree(TreeNode* root, int space) {
+    if (root == NULL) {
+        return;
+    }
+
+    space += 5; // Increase the distance between levels
+
+    // Print the right subtree
+    printTree(root->right, space);
+
+    // Print the current node
     printf("\n");
+    for (int i = 5; i < space; i++) {
+        printf(" ");
+    }
+    printf("%d\n", root->data);
+
+    // Print the left subtree
+    printTree(root->left, space);
 }
 
-//main function with switch cases
-
-int main()
-{
-    int n;
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
-    int *tree = (int*)malloc(n*sizeof(int));
-    for(int i=0; i<n; i++)
-        tree[i] = -1;
+int main() {
+    TreeNode* root = NULL;
     int choice, value;
-    while(1)
-    {
+
+    while (1) {
         printf("1. Insert\n");
         printf("2. Search\n");
         printf("3. Delete\n");
         printf("4. Inorder Traversal\n");
         printf("5. Preorder Traversal\n");
         printf("6. Postorder Traversal\n");
-        printf("7. Print\n");
+        printf("7. Print Tree\n");
         printf("8. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch(choice)
-        {
+
+        switch (choice) {
             case 1:
                 printf("Enter the value to be inserted: ");
                 scanf("%d", &value);
-                insert(tree, value, 0);
+                root = insert(root, value);
                 break;
             case 2:
                 printf("Enter the value to be searched: ");
                 scanf("%d", &value);
-                search(tree, value, 0);
+                TreeNode* result = search(root, value);
+                if (result != NULL) {
+                    printf("Found\n");
+                } else {
+                    printf("Not found\n");
+                }
                 break;
             case 3:
                 printf("Enter the value to be deleted: ");
                 scanf("%d", &value);
-                delete(tree, value, 0);
+                root = delete(root, value);
                 break;
             case 4:
-                inorder(tree, 0);
+                printf("Inorder Traversal: ");
+                inorder(root);
                 printf("\n");
                 break;
             case 5:
-                preorder(tree, 0);
+                printf("Preorder Traversal: ");
+                preorder(root);
                 printf("\n");
                 break;
             case 6:
-                postorder(tree, 0);
+                printf("Postorder Traversal: ");
+                postorder(root);
                 printf("\n");
                 break;
             case 7:
-                print(tree, n);
+                printf("Binary Tree Structure:\n");
+                printTree(root, 0);
                 break;
             case 8:
-                exit(0);
+                freeTree(root); // Free allocated memory
+                return 0;
             default:
                 printf("Invalid choice\n");
         }
     }
-    return 0;
 }
-
