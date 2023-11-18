09274af09555065b3718103510f19575a8aac4e2
Dhakkshin S R <srdhakkshin04@gmail.com>
2023-11-18 14:05:33 +0530

    min heap

---
+++ Dhakkshin S R/edgeDetection.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getGradients(image, kernel):
+    height, width = image.shape
+    k_height, k_width = kernel.shape
+    output = np.zeros((height - k_height + 1, width - k_width + 1))
+
+    for i in range(height - k_height + 1):
+        for j in range(width - k_width + 1):
+            output[i, j] = np.sum(image[i:i+k_height, j:j+k_width] * kernel)
+
+    return output
+
+# Load the image
+image_path = r'D:\College\DSA\Data-Structures-and-Algorithms\Dhakkshin S R\test2.jpg' 
+original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Sobel kernels
+sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+# Calculatiing the x anf y gradients of the image with Sobel kernels
+sobel_x_result = getGradients(original_image, sobel_x)
+sobel_y_result = getGradients(original_image, sobel_y)
+
+# Calculate the magnitude of the gradient
+gradient_magnitude = np.sqrt(sobel_x_result**2 + sobel_y_result**2)
+
+# Save the final result
+output_path = r'D:\College\DSA\Data-Structures-and-Algorithms\Dhakkshin S R\gradient_magnitude.jpg' 
+cv2.imwrite(output_path, gradient_magnitude)
+
+# Display the original image
+# plt.imshow(gradient_magnitude, cmap='gray')
+# plt.title('Gradient Magnitude')
+# plt.axis('off')
+# plt.show()
--- Dhakkshin S R/trees/bstLL.c
+++ Dhakkshin S R/trees/bstLL.c
@@ -10,12 +10,6 @@ struct node
     struct node * right;
 };
 
-int main(void)
-{
-
-
-}
-
 struct node* insertNode(struct node* root, int data)
 {   
     if (root == NULL)
@@ -28,10 +22,17 @@ struct node* insertNode(struct node* root, int data)
 
     while (iter)
     {
-        if (data > iter->data) 
+        if (data < iter->data) 
         {
-            parent = iter;
-            iter = iter->left;
+            if (iter->left)
+            {
+                iter = iter->left;
+            }
+            else
+            {
+                
+            }
+           
         }
         else 
         {
@@ -52,8 +53,39 @@ struct node* getNode(int data)
     return new;
 }
 
+struct node* newTree()
+{
+    int num;
+    printf("How many elemnets do you want in your tree? ");
+    scanf("%d", &num);
+
+    struct node* tree = NULL;
+    for
+}
+
+int main(void)
+{
+
+
+}
 
 void bfsPrint(struct node* root)
 {
 
 }
+
+struct node* inorderPredecessor(struct node* node)
+{
+    struct node* curr = node -> right;
+
+    while(curr -> left)
+    {
+        curr = curr -> left;
+    }
+    return curr;
+}
+
+// case 3 - 2 children
+// 1. find the inorder successor of the node to be deleted (it will be compulsorily leaf node or node with only right child.)
+// 2. replace the value of the node to be deleted with the value in the inorder successor.
+// 3. delete the inorder successor.
